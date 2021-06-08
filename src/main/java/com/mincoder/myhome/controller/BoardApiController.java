205b8b8651d6056c72126cac9db2a39625752ce6
Bminju <minjuuuz@gmail.com>
2021-06-09 01:56:08 +0900

    restAPI 적용한 BoardApiController 생성

---
+++ src/main/java/com/mincoder/myhome/controller/BoardApiController.java
@@ -0,0 +1,55 @@
+package com.mincoder.myhome.controller;
+
+
+import com.mincoder.myhome.model.Board;
+import com.mincoder.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository repository;
+
+    @GetMapping("/boards")
+    List<Board> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return repository.save(newBoard);
+    }
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElse(null);
+    }
+
+    @PostMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(board -> {
+                    board.setTitle(newBoard.getTitle());
+                    board.setContent(newBoard.getContent());
+                    return repository.save(board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return repository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/board/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+
+}
